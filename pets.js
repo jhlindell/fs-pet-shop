edc51416cd6cdda1618467627b1e3d52acdd7b5b
Jon Lindell <jhlindell@yahoo.com>
2017-05-23 16:37:37 -0600

    add read pets function

--- pets.js
+++ pets.js
@@ -1,6 +1,36 @@
-let argLength = process.argv.length;
-if(argLength<3){
-console.error("Usage: node pets.js [read | create | update | destroy]");
-} else {
-  console.log("success!");
+const fs = require('fs');
+switch(process.argv[2]){
+  case 'read':
+    readPets();
+    break;
+  case 'create':
+    break;
+  case 'update':
+
+    break;
+  case 'destroy':
+
+    break;
+  default:
+    console.error("Usage: node pets.js [read | create | update | destroy]");
+}
+
+function readPets(){
+  fs.readFile('./pets.json', 'utf8', function(err, res){
+    response = JSON.parse(res);
+    if(!process.argv[3]){
+      if (err){
+        console.error(err);
+      } else {
+        console.log(response);
+      }
+    } else {
+      let index = process.argv[3];
+      if(index > -1 && index < response.length) {
+        console.log(response[process.argv[3]]);
+      } else {
+        console.error("Usage: node pets.js read INDEX");
+      }
+    }
+  });
 }

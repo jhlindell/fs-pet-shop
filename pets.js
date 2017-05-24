dec6390788b111670a40c42b0bffa25e2b37e402
Brennen Barret Bull <brennenbull@Brennens-MacBook-Pro.local>
2017-05-24 14:29:07 -0600

    add error handling

--- pets.js
+++ pets.js
@@ -14,6 +14,7 @@ switch(process.argv[2]){
     break;
   default:
     console.error("Usage: node pets.js [read | create | update | destroy]");
+    process.exit(1);
 }
 
 function readPets(){
@@ -31,6 +32,7 @@ function readPets(){
         console.log(response[process.argv[3]]);
       } else {
         console.error("Usage: node pets.js read INDEX");
+        process.exit(1);
       }
     }
   });
@@ -38,19 +40,20 @@ function readPets(){
 
 function createPets(){
   if(!process.argv[3] || !process.argv[4] || !process.argv[5]){
-    console.error("Usage: node pets.js create AGE KIND NAME");
+    console.error('Usage: node pets.js create AGE KIND NAME');
+    process.exit(1);
   } else {
     fs.readFile('./pets.json', 'utf8', function(err, res){
       response = JSON.parse(res);
       let petObj = {};
-      petObj.age = process.argv[3];
+      petObj.age = Number(process.argv[3]);
       petObj.kind = process.argv[4];
       petObj.name = process.argv[5];
       response.push(petObj);
 
       fs.writeFile('pets.json', JSON.stringify(response), (err) => {
         if (err) throw err;
-        console.log('The file has been saved!');
+        console.log(petObj);
       });
     });
   }

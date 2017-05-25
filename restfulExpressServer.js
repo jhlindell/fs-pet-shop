f8820a109deb973391d0b107fd65684d53d17bb7
Jon Lindell <jhlindell@yahoo.com>
2017-05-25 15:09:58 -0600

    add error handling and 404 handling

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -5,6 +5,23 @@ const router = require("./restfulRoutes");
 
 app.use("/pets", router);
 
+// app.get('/', function(req, res, next) {
+//   Item.find(function(err, items) {
+//     if (err) return next(err);
+//     console.log(items);
+//   });
+// });
+
+// app.use(function(err, req, res, next) {
+//   console.error(err.stack);
+//   return res.send(500, { message: err.message });
+// });
+
+app.use(function (req, res) {
+  res.sendStatus(404);
+  // res.status(404).send("Not Found");
+});
+
 app.listen(port, function() {
   console.log('Listening on port', port);
 });
